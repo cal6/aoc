709b3fd8456e73953af4bf5ac768c9f21d2161ba
Kalle Volkov <kalle.volkov@starship.xyz>
2020-12-05 09:35:38 +0200

    day 4 - improve formatting

--- 2020/d05.js
+++ 2020/d05.js
@@ -4,8 +4,8 @@ const input = fs.readFileSync('input_05.txt').toString().split("\n");
 const getRow = (str) => {
 	let rows = [...Array(128).keys()];
 	for (const d of str) {
-		if (d === 'F') { rows = rows.slice(0, rows.length / 2) }
-		if (d === 'B') { rows = rows.slice(rows.length / 2, rows.length) }
+		if (d === 'F') { rows = rows.slice(0, rows.length / 2); }
+		if (d === 'B') { rows = rows.slice(rows.length / 2, rows.length); }
 	}
 	return rows[0]
 }
@@ -13,13 +13,14 @@ const getRow = (str) => {
 const getSeat = (str) => {
 	let seats = [...Array(8).keys()];
 	for (const d of str) {
-		if (d === 'L') { seats = seats.slice(0, seats.length / 2) }
-		if (d === 'R') { seats = seats.slice(seats.length / 2, seats.length) }
+		if (d === 'L') { seats = seats.slice(0, seats.length / 2); }
+		if (d === 'R') { seats = seats.slice(seats.length / 2, seats.length); }
 	}
-	return seats[0]
+	return seats[0];
 }
 
-let ids = []
+let ids = [];
+
 part1: {
 	let max_id = 0;
 	for (const ticket of input) {
