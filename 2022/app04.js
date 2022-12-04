ac7ebee119af80ced072acd3f95973587c1e9734
Kalle Volkov <kalle.volkov@hiirepadi.ee>
2022-12-04 11:37:54 +0200

    2022/04 - more compact result

--- 2022/app04.js
+++ 2022/app04.js
@@ -3,18 +3,10 @@ const fs = require('fs');
 const data = fs.readFileSync('data04.txt').toString('utf8').split("\n").map(r => r.split(',').map(p => p.split('-').map(n => parseInt(n, 10))));
 
 {
-	let res = 0;
-	data.forEach(p => {
-		if (p[0][0] >= p[1][0] && p[0][1] <= p[1][1] || p[1][0] >= p[0][0] && p[1][1] <= p[0][1]) res++;
-	});
-	console.log('Result 1', res);
+	console.log('Result 1', data.map(p => p[0][0] >= p[1][0] && p[0][1] <= p[1][1] || p[1][0] >= p[0][0] && p[1][1] <= p[0][1] ? 1 : 0).reduce((s, a) => s + a, 0));
 }
 
 
 {
-	let res = 0;
-	data.forEach(p => {
-		if (p[0][0] >= p[1][0] && p[0][0] <= p[1][1] || p[1][0] >= p[0][0] && p[1][0] <= p[0][1] || p[0][1] >= p[1][0] && p[0][0] <= p[1][1] || p[1][1] >= p[0][0] && p[1][0] <= p[0][1]) res++;
-	});
-	console.log('Result 2', res);
+	console.log('Result 2', data.map(p => p[0][0] >= p[1][0] && p[0][0] <= p[1][1] || p[1][0] >= p[0][0] && p[1][0] <= p[0][1] || p[0][1] >= p[1][0] && p[0][0] <= p[1][1] || p[1][1] >= p[0][0] && p[1][0] <= p[0][1] ? 1 : 0).reduce((s, a) => s + a, 0));
 }
