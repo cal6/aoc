71c329f935ebca0b405e347acc02796f4b71d902
Kalle Volkov <kalle.volkov@hiirepadi.ee>
2022-12-09 16:11:26 +0200

    2022/09 - more compact code

--- 2022/app09.js
+++ 2022/app09.js
@@ -11,40 +11,29 @@ const isSelf = (x1, y1, x2, y2) => {
 
 let x = new Array(10).fill(0);
 let y = new Array(10).fill(0);
-let tgrid = [];
-for (let i = 0; i <= 9; i++) {
-	tgrid[i] = [];
-}
+let tgrid = Array.from(Array(10), () => new Set())
 
 data.map(row => {
 	const n = parseInt(row[1], 10)
 	let d = row[0];
 	for (let s = 0; s < n; s++) {
-		if (d === 'U') { y[0]--; }
-		if (d === 'D') { y[0]++; }
-		if (d === 'L') { x[0]--; }
-		if (d === 'R') { x[0]++; }
-		for (let i = 1; i <= 9; i++) { 
-			if (isSelf(x[i-1], y[i-1], x[i], y[i]) || isNeighbour(x[i-1], y[i-1], x[i], y[i])) break;
-			if (x[i-1] > x[i]) x[i]++;
-			if (x[i-1] < x[i]) x[i]--;
-			if (y[i-1] > y[i]) y[i]++;
-			if (y[i-1] < y[i]) y[i]--;
-		}
-		for (let i = 1; i <= 9; i++) {
-			if (!tgrid[i][x[i]+'-'+y[i]]) { tgrid[i][x[i]+'-'+y[i]] = true; }
+		for (let i = 0; i <= 9; i++) { 
+			if (i > 0 && (isSelf(x[i-1], y[i-1], x[i], y[i]) || isNeighbour(x[i-1], y[i-1], x[i], y[i]))) break;
+			if (d === 'R' || x[i-1] > x[i]) x[i]++;
+			if (d === 'L' || x[i-1] < x[i]) x[i]--;
+			if (d === 'D' || y[i-1] > y[i]) y[i]++;
+			if (d === 'U' || y[i-1] < y[i]) y[i]--;
+			tgrid[i].add(x[i]+'-'+y[i]);
 		}
 	}
 });
 
 
 {
-	res = Object.keys(tgrid[1]).length;
-	console.log('Result 1', res);
+	console.log('Result 1', tgrid[1].size);
 }
 
 
 {
-	res = Object.keys(tgrid[9]).length;
-	console.log('Result 2', res);
+	console.log('Result 2', tgrid[9].size);
 }
