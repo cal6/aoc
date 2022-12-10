187a7d7c96fdea316234aded630a3c6241a8fdb8
Kalle Volkov <kalle.volkov@hiirepadi.ee>
2022-12-10 19:32:06 +0200

    2022/09 - more compact

--- 2022/app09.js
+++ 2022/app09.js
@@ -2,11 +2,8 @@ const fs = require('fs');
 
 const data = fs.readFileSync('data09.txt').toString('utf8').split("\n").map(r => r.split(' '));
 
-const isNeighbour = (x1, y1, x2, y2) => {
-	return Math.abs(x1-x2) <= 1 && Math.abs(y1-y2) <= 1;
-}
-const isSelf = (x1, y1, x2, y2) => {
-	return x1 === x2 && y1 === y2;
+const isClose = (x1, y1, x2, y2) => {
+	return (Math.abs(x1-x2) <= 1 && Math.abs(y1-y2) <= 1) || (x1 === x2 && y1 === y2);
 }
 
 let x = new Array(10).fill(0);
@@ -18,7 +15,7 @@ data.map(row => {
 	let d = row[0];
 	for (let s = 0; s < n; s++) {
 		for (let i = 0; i <= 9; i++) { 
-			if (i > 0 && (isSelf(x[i-1], y[i-1], x[i], y[i]) || isNeighbour(x[i-1], y[i-1], x[i], y[i]))) break;
+			if (i > 0 && isClose(x[i-1], y[i-1], x[i], y[i])) break;
 			if (d === 'R' || x[i-1] > x[i]) x[i]++;
 			if (d === 'L' || x[i-1] < x[i]) x[i]--;
 			if (d === 'D' || y[i-1] > y[i]) y[i]++;
